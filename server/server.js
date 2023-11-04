b1ff53cce85cb5e9c09b042e0b11e41d04415495
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-11-03 20:03:51 -0700

    webpack

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
