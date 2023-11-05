fb791dd8c26c3f49dcda1153ff9591ad328f120b
StephCummins <steph.a.cummins@gmail.com>
2023-11-05 09:53:44 -0800

    Updated main menu

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -2,19 +2,36 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const FrontPage = () => {
+const FrontPage = ({username}) => {
   const navigate = useNavigate();
   const createSession = () => {
     navigate('/createsession');
   }
   return (
-    <>
-      <h1>What would you like to learn today?</h1>
-      <button type='button' onClick={createSession}>CREAT NEW STUDY SESSION</button>
-      <button type='button'>REVIEW PAST STUDY SESSIONS</button>
-      <button type='button'>CREAT FLASH CARDS</button>
-      <button type='button'>STUDY FLASH CARDS</button>
-    </>
+    <div className="mainMenu">
+      <nav className="headerMenu">
+        <img src="./logo_small.png"/>
+        <ul>
+          <li className="username">WELCOME{username}</li>
+          <li>LOGOUT</li>
+        </ul>
+      </nav>
+      <section className="selectionMenu">
+        <h1>What would you like to learn today?</h1>
+        <div className="selectionButtons">
+          <button className="gridItem" type='button' onClick={createSession}>CREATE STUDY SESSION</button>
+          <button className="gridItem" type='button'>REVIEW STUDY SESSIONS</button>
+          <button className="gridItem" type='button'>CREATE FLASH CARDS</button>
+          <button className="gridItem" type='button'>STUDY FLASH CARDS</button>
+        </div>
+      </section>
+      <nav className="footerMenu">
+        <ul>
+          <li>ABOUT</li>
+          <li>HELP</li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 export default FrontPage;
