82a2c6d2094d583a3d8fa680a6194e44704c3d98
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-11-04 20:26:19 -0700

    Revert "1"
    This reverts commit fdfa4b88dabda08a51c885ef40c0d487b2b6d119.
    
    revert merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage.jsx";
 import SignupPage from "./components/SignupPage.jsx";
 import FrontPage from "./components/FrontPage.jsx";
